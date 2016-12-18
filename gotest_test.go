79889e33e25662a244d8507b858273777604bb0d
Martin Riedl <martin.riedl.92@googlemail.com>
2016-12-18 15:04:35 +0100

    new unit tests for error paths

--- gotest_test.go
+++ gotest_test.go
@@ -8,3 +8,17 @@ func TestMissingValues(t *testing.T) {
 		t.Error("missing values check failed")
 	}
 }
+
+func TestMissingValueFail1(t *testing.T) {
+	obj := GoTest{Val1: "A"}
+	if obj.Test() {
+		t.Error("val2 is missing! function Test should return false")
+	}
+}
+
+func TestMissingValueFail2(t *testing.T) {
+	obj := GoTest{Val2: "B"}
+	if obj.Test() {
+		t.Error("val1 is missing! function Test should return false")
+	}
+}
